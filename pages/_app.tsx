0ef1e281100611a17ec838c7c4a50120781d3352
Sagar Vispute <sagarvispute33@gmail.com>
2023-08-24 13:24:07 +0530

    Init structure

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app'
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import './global.scss'
+
+export default function App({ Component, pageProps }: AppProps) {
+    return <>
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+    </>
+}
