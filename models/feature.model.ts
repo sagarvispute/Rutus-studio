97ddbe402e70be1c79f1ba69f00b8e73bfdbca03
Sagar Vispute <sagarvispute33@gmail.com>
2023-08-24 16:31:21 +0530

    Banner and services creation

---
+++ models/feature.model.ts
@@ -0,0 +1,5 @@
+export interface Feature {
+    id: string,
+    title: string,
+    description: string
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Banner from '../components/Banner'
+import HomeFeature from '../components/HomeFeature'
 
 export default function index() {
   return (
     <div>
-      
+      <Banner />
+
+      <HomeFeature />
       home
       
     </div>
